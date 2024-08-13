43d87f1b4e950a94b3024aae8895303c1ad17679
Dakssh bhambre <Dakssh.bhambre@gmail.com>
2024-08-13 21:30:59 +0530

    FINALLY!!!! fixed write data to realtime Database

--- Auth.js
+++ Auth.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword,} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
 import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
 const firebaseConfig = {
   apiKey: "AIzaSyC4Q5C-rHwqJTHdkrkfr3TaHx_O8l2A50Y",
@@ -16,27 +16,34 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db = getDatabase(app);
+var db = getDatabase(app);
 
-let username = document.getElementById("user");
+function AddData() {
+const user = document.getElementById("user");
+set(ref(db, 'USERS/'), {
+  username: user.value
+}).then(() => {
+  alert("Data added successfully");
+}).catch((error) => {
+  alert(errorMessage);
+});
+}
 
-
-const submit = document.getElementById("submitregister");
-submit.addEventListener("click", function (event) {
+const submit = document.getElementById("submit");
+submit.addEventListener("click", AddData, function (event) {
   event.preventDefault();
+ 
   //inputs
   const email = document.getElementById("emailid").value;
   const password = document.getElementById("password").value;
-  
+
   //create user
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
       // Signed up
-      const user = userCredential.user;
-      // ...
-      alert("Successfully signed up!");
+      const user = userCredential.user; 
       window.location.href = "./App.html";
-      
+     
     })
     .catch((error) => {
       const errorCode = error.code;
@@ -46,7 +53,9 @@ submit.addEventListener("click", function (event) {
     });
 });
 
-const submitlog = document.getElementById("submit");
+
+
+const submitlog = document.getElementById("submitlog");
 submitlog.addEventListener("click", function (event) {
   event.preventDefault();
   //inputs
